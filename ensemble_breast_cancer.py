92965344f011f8eed4da15e8c180fd04ac1338bb
avp38 <avp38@njit.edu>
2017-04-09 03:41:20 -0400

    Added ensemble classifier v4

--- ensemble_breast_cancer.py
+++ ensemble_breast_cancer.py
@@ -8,12 +8,22 @@ import shutil
 
 from pyspark import SparkContext
 from pyspark.mllib.classification import LogisticRegressionWithSGD
+from pyspark.mllib.classification import SVMWithSGD
+from pyspark.mllib.classification import NaiveBayes
+from pyspark.mllib.tree import RandomForest
+from pyspark.mllib.tree import GradientBoostedTrees
+from pyspark.mllib.regression import LinearRegressionWithSGD
 from pyspark.mllib.regression import LabeledPoint
 from numpy import array
 
 # DEBUG environment
 DEBUG = 0
 
+# Parameters
+train_file = "/home/ajit/Desktop/spark_lib/dataset/breast_cancer.data.train"
+test_file = "/home/ajit/Desktop/spark_lib/dataset/breast_cancer.data.test"
+ensemble_train_file = "/home/ajit/Desktop/spark_lib/intermediate_output/breast_cancer.ens.train"
+ensemble_test_file = "/home/ajit/Desktop/spark_lib/intermediate_output/breast_cancer.ens.test"
 
 # Configure the environment                                                     
 if 'SPARK_HOME' not in os.environ:
@@ -26,15 +36,29 @@ def parsePoint(line):
 
 def appendColumn(ensemble_data, label_data):
 	for i in range(0,len(ensemble_data),1):
-		ensemble_data[i].extend(label_data[i])
+		ensemble_data[i].extend([label_data[i]])
+
+
+def writeToFile(en_data, filename):
+	f = open(filename,'w+')
+	for r in range(0,len(en_data),1):
+		for c in range(0,len(en_data[0]),1):		    
+		    if (c == len(en_data[0])-1):
+		    	if(r==(len(en_data)-1)):
+		    		f.write(str(en_data[r][c]))
+		    	else:
+		        	f.write(str(en_data[r][c])+"\n")
+	        else:
+	        	f.write(str(en_data[r][c])+" ")
+	f.close()
 
 
 sc = SparkContext(appName="Ensemble_Classifier_Breast_Cancer_Dataset")
 
-data = sc.textFile("/home/ajit/Desktop/spark_lib/dataset/breast_cancer.data.train")
+data = sc.textFile(train_file)
 train_data = data.map(parsePoint)
 
-data = sc.textFile("/home/ajit/Desktop/spark_lib/dataset/breast_cancer.data.test")
+data = sc.textFile(test_file)
 test_data = data.map(parsePoint)
 
 
@@ -58,6 +82,8 @@ for i in range(0, len(train_labels),1):
 	ensemble_train.append(l1) 
 
 
+
+
 # C1 
 # Build the Model
 model = LogisticRegressionWithSGD.train(train_data)
@@ -71,7 +97,7 @@ c1_predict_labels_train = c1_predict_labels_train_rdd.collect()
 # Append Labels
 appendColumn(ensemble_test, c1_predict_labels_test)
 appendColumn(ensemble_train, c1_predict_labels_train)
-
+print(ensemble_test)
 
 # C2 
 # Build the Model
@@ -86,7 +112,8 @@ c1_predict_labels_train = c1_predict_labels_train_rdd.collect()
 # Append Labels
 appendColumn(ensemble_test, c1_predict_labels_test)
 appendColumn(ensemble_train, c1_predict_labels_train)
-
+print("---------------------")
+print(ensemble_test)
 
 # C3 
 # Build the Model
@@ -143,6 +170,222 @@ c1_predict_labels_train_rdd = train_data.map(lambda p: ( model.predict(p.feature
 c1_predict_labels_test = c1_predict_labels_test_rdd.collect()
 c1_predict_labels_train = c1_predict_labels_train_rdd.collect()
 
+# Append Labels
+appendColumn(ensemble_test, c1_predict_labels_test)
+appendColumn(ensemble_train, c1_predict_labels_train)
 
+
+# SVM
+# C7 
+# Build the Model
+model = SVMWithSGD.train(train_data, regParam=0.01, regType='l2')
+
+# Predict Labels
+c1_predict_labels_test_rdd = test_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_train_rdd = train_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_test = c1_predict_labels_test_rdd.collect()
+c1_predict_labels_train = c1_predict_labels_train_rdd.collect()
+
+# Append Labels
+appendColumn(ensemble_test, c1_predict_labels_test)
+appendColumn(ensemble_train, c1_predict_labels_train)
+
+# C8 
+# Build the Model
+model = SVMWithSGD.train(train_data, regParam=0.1, regType='l2')
+
+# Predict Labels
+c1_predict_labels_test_rdd = test_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_train_rdd = train_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_test = c1_predict_labels_test_rdd.collect()
+c1_predict_labels_train = c1_predict_labels_train_rdd.collect()
+
+# Append Labels
+appendColumn(ensemble_test, c1_predict_labels_test)
+appendColumn(ensemble_train, c1_predict_labels_train)
+
+
+# C9 
+# Build the Model
+model = SVMWithSGD.train(train_data, regParam=0.001, regType='l1')
+
+# Predict Labels
+c1_predict_labels_test_rdd = test_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_train_rdd = train_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_test = c1_predict_labels_test_rdd.collect()
+c1_predict_labels_train = c1_predict_labels_train_rdd.collect()
+
+# Append Labels
+appendColumn(ensemble_test, c1_predict_labels_test)
+appendColumn(ensemble_train, c1_predict_labels_train)
+
+
+# C10 
+# Build the Model
+model = SVMWithSGD.train(train_data, regParam=0.1, regType='l1')
+
+# Predict Labels
+c1_predict_labels_test_rdd = test_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_train_rdd = train_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_test = c1_predict_labels_test_rdd.collect()
+c1_predict_labels_train = c1_predict_labels_train_rdd.collect()
+
+# Append Labels
+appendColumn(ensemble_test, c1_predict_labels_test)
+appendColumn(ensemble_train, c1_predict_labels_train)
+
+
+# C11
+# Build the Model
+model = SVMWithSGD.train(train_data, regParam=1, regType='l1')
+
+# Predict Labels
+c1_predict_labels_test_rdd = test_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_train_rdd = train_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_test = c1_predict_labels_test_rdd.collect()
+c1_predict_labels_train = c1_predict_labels_train_rdd.collect()
+
+# Append Labels
+appendColumn(ensemble_test, c1_predict_labels_test)
+appendColumn(ensemble_train, c1_predict_labels_train)
+
+
+# C12 
+# Build the Model
+model = SVMWithSGD.train(train_data, regParam=10, regType='l1')
+
+# Predict Labels
+c1_predict_labels_test_rdd = test_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_train_rdd = train_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_test = c1_predict_labels_test_rdd.collect()
+c1_predict_labels_train = c1_predict_labels_train_rdd.collect()
+
+# Append Labels
+appendColumn(ensemble_test, c1_predict_labels_test)
+appendColumn(ensemble_train, c1_predict_labels_train)
+
+
+# Naive Bayes
+# C13 
+# Build the Model
+model = NaiveBayes.train(train_data, lambda_=1.0)
+
+# Predict Labels
+c1_predict_labels_test_rdd = test_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_train_rdd = train_data.map(lambda p: ( model.predict(p.features)))
+c1_predict_labels_test = c1_predict_labels_test_rdd.collect()
+c1_predict_labels_train = c1_predict_labels_train_rdd.collect()
+
+# Append Labels
+appendColumn(ensemble_test, c1_predict_labels_test)
+appendColumn(ensemble_train, c1_predict_labels_train)
+
+
+# Feature Data for Random Forest and Boosted Trees (List of Lists)
+# Read trainfile
+f = open(train_file)
+train_features = []
+l = f.readline()
+
+while(l!=''):
+    a = l.split()
+    l2 = []
+    # Note: Started with index 1 to avoid the label
+    for j in range(1,len(a),1):
+        l2.extend([float(a[j])])
+    train_features.append(l2)
+    l = f.readline()
+
+f.close()
+
+# Read testfile
+f = open(test_file)
+test_features = []
+l = f.readline()
+
+while(l!=''):
+    a = l.split()
+    l2 = []
+    for j in range(1,len(a),1):
+        l2.extend([float(a[j])])
+    test_features.append(l2)
+    l = f.readline()
+
+f.close()
+
+
+# Random Forest
+# C14 - C21
+# Build the Model
+rf_trees = [25, 50, 100, 150, 200, 250, 300]
+
+for i in range(0, len(rf_trees), 1):
+	num_rf_trees = rf_trees[i]
+
+	# Build the Model
+	model = RandomForest.trainClassifier(train_data, 2, {}, num_rf_trees)
+
+	rf_train_predict_label = []
+	rf_test_predict_label = []
+
+	# Predict Labels
+	for j in range(0, len(test_features), 1):
+		p_l = model.predict(test_features[j])
+		rf_test_predict_label.extend([p_l])
+
+	for j in range(0, len(train_features), 1):
+		p_l = model.predict(train_features[j])
+		rf_train_predict_label.extend([p_l])
+
+
+	# Append Labels
+	appendColumn(ensemble_test, rf_test_predict_label)
+	appendColumn(ensemble_train, rf_train_predict_label)
+
+
+# Gradient Boosted Trees
+# C14 - C21
+# Build the Model
+# Build the Model
+model = GradientBoostedTrees.trainClassifier(train_data, {})
+
+gbt_train_predict_label = []
+gbt_test_predict_label = []
+
+# Predict Labels
+for j in range(0, len(test_features), 1):
+	p_l = model.predict(test_features[j])
+	gbt_test_predict_label.extend([p_l])
+
+for j in range(0, len(train_features), 1):
+	p_l = model.predict(train_features[j])
+	gbt_train_predict_label.extend([p_l])
+
+
+# Append Labels
+appendColumn(ensemble_test, gbt_test_predict_label)
+appendColumn(ensemble_train, gbt_train_predict_label)
+print("--------------")
+print(ensemble_test)
+print(len(ensemble_test))
+print(len(ensemble_test[0]))
+#----------
+# Save Intermediate Ensemble Output
+writeToFile(ensemble_train, ensemble_train_file)
+writeToFile(ensemble_test, ensemble_test_file)
+
+
+# Run Linear SVM on intermediate ensemble data
+data = sc.textFile(ensemble_train_file)
+ens_train_data = data.map(parsePoint)
+
+data = sc.textFile(ensemble_test_file)
+ens_test_data = data.map(parsePoint)
+
+# Build the Model & Predic labels for test data
+model = SVMWithSGD.train(ens_train_data)
+labelsAndPreds = ens_test_data.map(lambda p: (p.label, model.predict(p.features)))
+testErr = labelsAndPreds.filter(lambda (v,p): v!=p).count()/float(ens_test_data.count())
+print("Ensemble Classifier Error " + str(testErr))
 
 sc.stop() 
--- test_ensemble.py
+++ test_ensemble.py
@@ -8,9 +8,12 @@ import shutil
 
 from pyspark import SparkContext
 from pyspark.mllib.classification import LogisticRegressionWithSGD
+from pyspark.mllib.classification import SVMWithSGD
+from pyspark.mllib.classification import NaiveBayes
+from pyspark.mllib.tree import RandomForest
 from pyspark.mllib.regression import LabeledPoint
 from numpy import array
-
+import numpy as np
 
 # Configure the environment                                                     
 if 'SPARK_HOME' not in os.environ:
@@ -26,22 +29,28 @@ def appendColumn(ensemble_data, label_data):
 		ensemble_data[i].extend(label_data[i])
 
 
-sc = SparkContext(appName="Ensemble_Classifier_Test")
+# Parameters
+train_file = "/home/ajit/Desktop/spark_lib/dataset/breast_cancer.data.train"
+test_file = "/home/ajit/Desktop/spark_lib/dataset/breast_cancer.data.test"
 
-data = sc.textFile("/home/ajit/Desktop/spark_lib/dataset/breast_cancer.data.train")
-train_data = data.map(parsePoint)
 
-data = sc.textFile("/home/ajit/Desktop/spark_lib/dataset/breast_cancer.data.test")
-test_data = data.map(parsePoint)
+# sc = SparkContext(appName="Ensemble_Classifier_Test")
+
+# data = sc.textFile("/home/ajit/Desktop/spark_lib/dataset/breast_cancer.data.train")
+# train_data = data.map(parsePoint)
+
+# data = sc.textFile("/home/ajit/Desktop/spark_lib/dataset/breast_cancer.data.test")
+# test_data = data.map(parsePoint)
 
 # Build the Model
-model = LogisticRegressionWithSGD.train(train_data, regParam=10, regType='none')
-
-
+model = RandomForest.trainClassifier(train_data, 2, {}, 100)
+temp = model.predict([13.4, 20.52, 88.64, 556.7, 0.1106, 0.1469, 0.1445, 0.08172, 0.2116, 0.07325, 0.3906, 0.9306, 3.093, 33.67, 0.00541, 0.02265, 0.03452, 0.01334, 0.01705, 0.004, 16.41, 29.66, 113.3, 844.4, 0.1574, 0.3856, 0.5106, 0.2051, 0.3585, 0.1109])
+print(temp)
 # Evaluating model on training data
-labelsAndPreds = test_data.map(lambda p: (p.label, model.predict(p.features)))
-testErr = labelsAndPreds.filter(lambda (v,p): v!=p).count()/float(test_data.count())
-print("Error " + str(testErr))
-
+# labelsAndPreds = test_data.map(lambda p: (p.label, model.predict(p.features)))
+# print(labelsAndPreds.take(labelsAndPreds.count()))
+# testErr = labelsAndPreds.filter(lambda (v,p): v!=p).count()/float(test_data.count())
+# print("Error " + str(testErr))
+# print(labelsAndPreds.take(labelsAndPreds.count()))
 
 sc.stop()
